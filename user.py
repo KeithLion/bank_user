fa972b79a1322c7749bb375f9b8b3d0b09a04e43
Keith N <78460602+KeithLion@users.noreply.github.com>
2021-12-08 12:33:36 -0600

    added a transfer money method. had the first user tranfer 20 to the last user

--- user.py
+++ user.py
@@ -3,6 +3,7 @@ class User:
     def __init__(self,name,balance):
         self.name = name
         self.account_balance= balance
+        self.amount = 0
 
     def make_withdraw(self,amount):
         self.account_balance -= amount
@@ -14,11 +15,18 @@ class User:
         print(f'User:{self.name}, Balance: {self.account_balance}')
         return self
 
+    def transfer_balance(self, amount, user):
+        self.amount -= amount
+        user.amount += amount
+        self.display_balance()
+        user.display_balance()
+        # return self
+
 Alex = User('Alex', 100)
 Alex.deposit(100).deposit(20).deposit(80).make_withdraw(50).display_balance()
 Uin = User('Uin', 70)
 Uin.deposit(18).deposit(30).make_withdraw(15).make_withdraw(30).display_balance()
 Jake = User('Jake',200)
 Jake.deposit(40).make_withdraw(70).make_withdraw(25).make_withdraw(20).display_balance()
-
+Jake.transfer_balance(20, Alex)
 
