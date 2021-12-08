769c3bafb46f77a43bb88b02f8476ec53a14a847
Keith N <78460602+KeithLion@users.noreply.github.com>
2021-12-08 11:49:28 -0600

    added 2 instnaces and edited the first. ajusted balance attribute and fixed a spelling error

--- user.py
+++ user.py
@@ -1,11 +1,11 @@
 class User:
 
-    def __init__(self,name):
+    def __init__(self,name,balance):
         self.name = name
-        self.account_balance= 0
+        self.account_balance= balance
 
     def make_withdraw(self,amount):
-        self.acount_balance -= amount
+        self.account_balance -= amount
         return self
     def deposit(self,amount):
         self.account_balance += amount
@@ -15,6 +15,10 @@ class User:
         return self
 
 Alex = User('Alex', 100)
-Alex.make_withdraw(50)
-print(Alex.display_balance())
+Alex.deposit(100).deposit(20).deposit(80).make_withdraw(50).display_balance()
+Uin = User('Uin', 70)
+Uin.deposit(18).deposit(30).make_withdraw(15).make_withdraw(30).display_balance()
+Jake = User('Jake',200)
+Jake.deposit(40).make_withdraw(70).make_withdraw(25).make_withdraw(20).display_balance()
+
 
