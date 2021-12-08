3b2c9d3a71bb88f5b2011cb653d2ffd44ba0e394
Keith N <78460602+KeithLion@users.noreply.github.com>
2021-12-08 08:37:57 -0600

    created balance display method

--- user.py
+++ user.py
@@ -7,6 +7,8 @@ class User:
     def make_withdraw(self,amount):
         balance -= amount
         return self
-    def display_balance(self):
+    def deposit(self):
         balance += amount
         return self
+    def display_balance():
+        print(f'{self.name}, Balanc: {self.balance}')
