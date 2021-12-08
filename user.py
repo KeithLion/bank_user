84607b34885f1f2e21337d0df407fb7c04c32ee6
Keith N <78460602+KeithLion@users.noreply.github.com>
2021-12-08 09:44:42 -0600

    added balance to init. updated deposit method and display balance. called the user function

--- user.py
+++ user.py
@@ -1,14 +1,20 @@
 class User:
 
-    def __init__(self,name,balance):
+    def __init__(self,name):
         self.name = name
-        self.balance = balance
+        self.account_balance= 0
 
     def make_withdraw(self,amount):
-        balance -= amount
+        self.acount_balance -= amount
         return self
-    def deposit(self):
-        balance += amount
+    def deposit(self,amount):
+        self.account_balance += amount
         return self
-    def display_balance():
-        print(f'{self.name}, Balanc: {self.balance}')
+    def display_balance(self):
+        print(f'User:{self.name}, Balance: {self.account_balance}')
+        return self
+
+Alex = User('Alex', 100)
+Alex.make_withdraw(50)
+print(Alex.display_balance())
+
